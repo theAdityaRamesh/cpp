fec16c6b61bf7b1d98b6744ec05096dd52f5c081
Aditya Ramesh <mail.aditya.ramesh@gmail.com>
2021-09-29 18:29:07 +0530

    added comments

--- leetcode/permutations.cpp
+++ leetcode/permutations.cpp
@@ -3,14 +3,32 @@
 class Solution {
 public:
     
-    vector<vector<int>> result;
-    vector<int> rArrT, iArrT;
+    vector<vector<int>> result; // intialize results vector
+    vector<int> rArrT, iArrT; // intialize temp vectors
     
-    void gPermute
+    // the function gPermute works as follows
+    // gPermute([1 2 3], [])
+
+    //  gPermute([2 3], [1])
+
+        // gPermute([2], [1 3])
+    
+            // gPermute([], [1 3 2]) -> @ this pt rArr.size() == 0 so push and into result vector
+    
+        // gPermute([3], [1 2]])
+    
+            // gPermute([], [1 2 3])
+
+    //  gPermute([1 3], [2])
+
+    //  gPermute([1 2], [3])
+
+
+    void gPermute // helper function
         (vector<int> rArr, vector<int> iArr){
         
         if(rArr.empty()){            
-            result.push_back(iArr);
+            result.push_back(iArr); 
             return;
         }        
         
@@ -19,16 +37,13 @@ public:
             for(int i = 0; i < rArr.size(); i++){
 
                 rArrT = rArr;
-                rArrT.erase(rArrT.begin()+i);
+                rArrT.erase(rArrT.begin()+i); // [1 2 3] - > [2 3]
                 
-                iArrT = iArr;
-                iArrT.push_back(rArr[i]);
+                iArrT = iArr; // []
+                iArrT.push_back(rArr[i]); // [] -> [1]
                 
                    
-                gPermute(rArrT, iArrT );
-                
-
-                
+                gPermute(rArrT, iArrT ); // [2 3] , [1]                
             }
             
         }        
@@ -37,7 +52,7 @@ public:
     
     vector<vector<int>> permute(vector<int>& nums) {
         
-       
+       // calling the helper function
         
         if(nums.size() == 1){
             result.push_back(nums);
@@ -51,11 +66,30 @@ public:
     }
 };
 
+// method 2
+// more elegant
+// saves space due to non creationg of temp arrays
 
 class Solution { 
 
     public:
     
+    // [ 1 2 3] indx = 0
+    // [1 2 3] i=0,indx=0  ; [2 1 3] i=1,indx=0 ; [ 3 2 1] i=2,indx=0
+    
+    // [1 2 3] i=0,indx=0  ;
+    //      [ 1 2 3] i=1,indx=1 ; [ 1 3 2] i=2,indx=1 
+
+    // [2 1 3] i=1,indx=0
+    //      [2 1 3] i=1,indx=1 ; [2 3 1] i=2, indx=1
+            // [ 2 1 3] i=2,indx=2;
+                // [2 1 3] indx=3 result.push_back([2 1 3])
+
+
+    // [3 2 1] i=2,indx=0
+    //      [3 2 1] i=1, indx=1 ; [ 3 1 2] i=2,indx=1
+
+
     void gPermute(int indx, vector<int> nums, vector<vector<int>>& result){
         
         if(indx == nums.size()){
