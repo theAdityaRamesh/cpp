83ba5ace0dc1c88156962821f65e7ba247ed579c
Aditya Ramesh <mail.aditya.ramesh@gmail.com>
2021-09-29 19:00:44 +0530

    Create trapping-rain-water.cpp

---
+++ leetcode/trapping-rain-water.cpp
@@ -0,0 +1,13 @@
+//leetcode.com/problems/trapping-rain-water/
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        // 0 0 0 0 0 0 0 1 0 0 0
+        // 0 0 0 1 * * * 1 * 1 0
+        // 0 1 * 1 1 * 1 1 1 1 0 ? substring matching algo lol ?
+        // find substring of type 1 0 ... 0 1
+        // iterate each level 
+        
+    }
+};
