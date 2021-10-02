49a4c86369be11703f08a6eadd215454f2c184bc
Aditya Ramesh <mail.aditya.ramesh@gmail.com>
2021-10-02 18:29:53 +0530

    Update trapping-rain-water.cpp

--- leetcode/trapping-rain-water.cpp
+++ leetcode/trapping-rain-water.cpp
@@ -1,13 +1,46 @@
 //leetcode.com/problems/trapping-rain-water/
 
 class Solution {
-public:
-    int trap(vector<int>& height) {
-        // 0 0 0 0 0 0 0 1 0 0 0
-        // 0 0 0 1 * * * 1 * 1 0
-        // 0 1 * 1 1 * 1 1 1 1 0 ? substring matching algo lol ?
-        // find substring of type 1 0 ... 0 1
-        // iterate each level 
+    public:
+    int trap(vector<int>& arr) {
+     
+        int max_indx = 0, max_element = -1, it = 0, water = 0;
         
+        while(it < arr.size()-1){
+            
+            if(arr[it] > arr[it+1]){
+                
+                int j = it+1;
+                max_indx = it+1;
+                
+                while(j < arr.size()){
+                    
+                    if(arr[j] > arr[it]){
+                        max_indx = j;
+                        break;
+                    } else if(arr[j] > arr[max_indx]){
+                        max_indx = j;
+                    }
+                
+                    j++;
+                }
+                
+                if(max_indx != it+1){
+                    int height = min(arr[it], arr[max_indx]);
+                    for(int k = it+1; k < max_indx; k++){
+                        water += height - arr[k];
+                    }
+                }
+                
+                it = max_indx;
+                continue;
+            }
+            
+            it++;
+        }
+            return water;
     }
+    
+    
 };
+
