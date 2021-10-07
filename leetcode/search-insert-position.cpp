0b11ee4ef6fa38c88abaec5e6a31cc2958f30e82
Aditya Ramesh <mail.aditya.ramesh@gmail.com>
2021-10-07 18:13:42 +0530

    Update search-insert-position.cpp

--- leetcode/search-insert-position.cpp
+++ leetcode/search-insert-position.cpp
@@ -6,33 +6,40 @@ public:
         
         if(target < arr[0] || arr.empty()){
             return 0;
-        } 
+        } // handles values larger than array
         
         if(target > arr[arr.size()-1]){
             return arr.size();
-        }
-        
-        if(target == arr[arr.size()-1]){
-            return arr.size()-1;
-        }
+        } // handles values smaller than array
+
+        // handles values in array
         
         int head = 0, tail = arr.size()-1, mid = (head+tail)*0.5;
         while(head < tail){
 
-            mid = (head+tail)/2;
-            cout << head << " " << mid << " " << tail << endl;
+            mid = head + (tail-head)/2;
+            // to avoid overflow
+            // for large arrays
             
             if(arr[mid] == target){
-                return mid;
+                return mid; // binary search
             } else {
                 if(arr[mid] < target){
-                    head = mid+1;
+                    head = mid+1; 
                 } else {
                     tail = mid-1;
                 }
             }
         }
-    
-        return mid;
+        
+        // now the loop breaks when head = tail
+        // now we choose to return head because mid does not get updated in the last iteration
+        // if the target is larger then head then it will be next to it
+            // as we have narrowed down the range to a single element
+            // head and tail represent the possible values in which the target can exist
+        // else it will be at head
+        
+        head = arr[head] < target ? head+1 : head;
+        return head;
     }
 };
