e14e9d819c4b2bf2c53387fe594bf50ba3a5c9d1
Aditya Ramesh <mail.aditya.ramesh@gmail.com>
2022-02-08 16:37:07 +0530

    Update container-with-most-water.cpp

--- leetcode/container-with-most-water.cpp
+++ leetcode/container-with-most-water.cpp
@@ -5,34 +5,14 @@ public:
         
         int left = 0,
             right = height.size()-1,
-            area = 0,
-            minHeight = min(height[left],height[right]),
-            maxArea = (right-left)*minHeight;
+            maxArea = 0;
         
        while(left < right){
-   
-            if(height[left] > height[right]){
-                while(height[right] <= minHeight && right > 0){
-                    right--;
-                }
-                
-                area = (right-left)*min(height[right],height[left]);
-                if(maxArea < area){
-                    maxArea = area;
-                }
-                
-            } else {
-                while(height[left] <= minHeight && left < height.size()-1){
-                    left++;
-                }
-                
-                area = (right-left)*min(height[right],height[left]);
-                if(maxArea < area){
-                    maxArea = area;
-                }
-                
-            }
-           minHeight = min(height[left],height[right]);
+           
+              maxArea = max((right-left)*min(height[right],height[left]), maxArea);      
+              if(height[left] > height[right]){
+                  right--;
+              } else { left++;}
        }
         return maxArea;
     }
