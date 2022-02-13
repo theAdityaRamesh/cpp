7e6ec1d6851e99fc421c3160b79307d5ad606865
Aditya Ramesh <mail.aditya.ramesh@gmail.com>
2022-02-13 12:31:42 +0530

    Update 4sum.cpp

--- leetcode/4sum.cpp
+++ leetcode/4sum.cpp
@@ -1,3 +1,81 @@
+// O(n^3) complexity recursive method
+// generalized sum for k sums
+
+class Solution {
+public:
+    
+    vector<vector<int>> twoSum(const vector<int>& nums, const int& target, const int& curr){
+
+        vector<vector<int>> res;
+        int left = curr;
+        int size = nums.size();
+        int right = size - 1;
+        
+        while(left < right){
+            
+            int sum = nums[left] + nums[right];
+            
+            if (sum < target || (left > curr && nums[left] == nums[left - 1])) {
+                ++left;
+            } else if (sum > target || (right < size - 1 && nums[right] == nums[right + 1])) {
+                --right;
+            } 
+            
+            else {
+                res.push_back({
+                    nums[left],
+                    nums[right]
+                });
+                
+                left++;
+                right--;                
+            }
+        }
+    
+        return res;
+    }    
+
+    vector<vector<int>> kSum(const vector<int>& nums, const int& target, const int& curr,  const int& k){
+        
+        vector<vector<int>> res;
+        
+        if(curr == nums.size()){
+            return res; 
+        }
+        
+        
+        int avg = target/k;
+        if( nums[curr] > avg || nums.back() < avg){
+            return res;
+        }
+        
+        if( k == 2){
+            return twoSum(nums, target, curr);
+        }
+        
+        for(int i = curr; i < nums.size(); i++){
+            if(i == curr || nums[i-1] != nums[i]){
+
+                for(vector<int>& subset : kSum(nums, target - nums[i], i + 1, k-1)){
+                    res.push_back({nums[i]});
+                    res.back().insert(res.back().end(), subset.begin(), subset.end());
+                }
+            
+            }
+        }
+        
+        return res;
+        
+    }
+    
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {        
+        sort(nums.begin(), nums.end());
+        return kSum(nums, target, 0, 4);
+    }
+};
+
+// O(n^3) complexity iterative method
+
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
