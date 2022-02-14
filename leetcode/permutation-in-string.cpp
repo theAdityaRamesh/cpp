daebf1d0ffef18bcb47217794a2955d6934d10a0
Aditya Ramesh <mail.aditya.ramesh@gmail.com>
2022-02-14 18:36:54 +0530

    Update permutation-in-string.cpp

--- leetcode/permutation-in-string.cpp
+++ leetcode/permutation-in-string.cpp
@@ -3,6 +3,62 @@
 
 class Solution {
 public:
+       
+    int returnIndx(char c){
+        return int(c)-97; // helper function 
+        // ascii of 'a' is 97
+    }
+    
+    bool checkInclusion(string s1, string s2) {
+        if(s1.length() > s2.length()) return false; // if second string is smaller it cant fit a permutation
+        
+        vector<int> track(26,0); // tracking array of len 26 for smallcase letter
+        
+        for(int i = 0;i < s1.size(); i++){
+            track[returnIndx(s1[i])]++;
+        } // count word freq in s1
+        
+        int start = 0;
+        int size = s1.size();
+        int end = start+size; // sliding window from start to end
+        bool output = false;
+        
+        for(int i = 0; i < s2.size(); i++){ // scan from start to end of second string
+            if( i == end){
+                // if we reach end of sliding windoe
+                output = true;
+                for(int c : track){
+                    if(c != 0){ // check if we have equal word frequency
+                        output = false;
+                        break; // if not break
+                    }
+                } if(output) return output;
+                
+                track[returnIndx(s2[end++])]--; // decrement word frequency of secind strnig
+                track[returnIndx(s2[start++])]++; // increment the char in the prev sliding window
+                
+            } else {
+                track[returnIndx(s2[i])]--; // if inside window decrease char frequency
+            }
+        }
+        
+        output = true; // if sliding windoe at end contains permutation loop will end
+        for(int c : track){ // hence check once more
+            if(c != 0){
+                output = false;
+                break;
+            }
+        } if(output) return output;
+        
+        return output;
+    }
+};
+
+// O(1) memory
+// O(n) time
+
+class Solution {
+public:
     
     int returnIndx(char c){
         return int(c) - 97;
