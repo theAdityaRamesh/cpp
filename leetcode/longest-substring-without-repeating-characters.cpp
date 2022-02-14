2c755e31e957c8305f65f9b0546d7e8e191f615b
Aditya Ramesh <mail.aditya.ramesh@gmail.com>
2022-02-14 12:10:08 +0530

    Update longest-substring-without-repeating-characters.cpp

--- leetcode/longest-substring-without-repeating-characters.cpp
+++ leetcode/longest-substring-without-repeating-characters.cpp
@@ -4,25 +4,25 @@ class Solution {
 public:
     
     int indx(const char& c){
-        return int(c) - 32;
+        return int(c) - 32; // convert char to int from ascii table
     }
     
     int lengthOfLongestSubstring(string s) {
         int maxLen = 0;
         int currLen = 0;
-        vector<int> track(95,-1);
+        vector<int> track(95,-1); // all digits, spaces and capital and small letters sum to 95
         int start = 0;
         
         for (int it = 0; it < s.size(); it++){
             
-            int index = indx(s[it]);
-            if ( track[index] == -1 || track[index] < start ){
-                track[index] = it;
+            int index = indx(s[it]);// if match found
+            if ( track[index] == -1 || track[index] < start ){ // check for repitions by comparing start index and last occurance 
+                track[index] = it; // upadte last found index // if last occurance < start it means char was not present in current string
                 
             } else {
                 start = track[index] + 1;
-                track[index] = it;
-                
+                track[index] = it; // if match found then updaate max len
+                                    // restart the tracking
                 maxLen = maxLen < currLen ? currLen : maxLen;                
 
             }
