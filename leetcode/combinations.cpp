db45f73470c596f0ebf5e282539d98592ac279dd
Aditya Ramesh <mail.aditya.ramesh@gmail.com>
2022-02-17 15:51:02 +0530

    Update combinations.cpp

--- leetcode/combinations.cpp
+++ leetcode/combinations.cpp
@@ -1,3 +1,42 @@
+// iterative solution 
+
+class Solution {
+public:
+
+    vector<vector<int>> combine(int n, int k) {
+
+        // return n choose k
+        vector<vector<int>> res;
+        vector<int> ans(k, 0);
+        int indx = 0;
+//         0 0 0 
+//         1 0 0 
+//         1 1 0
+//         1 2 2
+//         1 2 3 -> push back i = 2
+//         1 2 4
+//         1 2 5 
+//         1 2 6 -> i--, 6 > n-k+i+1
+//         1 3 3 
+//         1 3 4 -> push back i = 2
+        
+        while( indx >= 0){            
+            ans[indx]++;
+            
+            if( ans[indx] > n-k+indx+1){
+                indx -= 1;
+            } else if(indx == k-1){
+                res.push_back(ans);
+            } else{
+                indx += 1;
+                ans[indx] = ans[indx-1];
+            }
+            
+        }
+        return res;
+    }
+};
+
 // recursive implementation
 
 class Solution {
