45ed0d35aa955bb833cfdd01cf31aea79c46b38a
Aditya Ramesh <mail.aditya.ramesh@gmail.com>
2022-02-19 22:59:31 +0530

    Update triangle.cpp

--- leetcode/triangle.cpp
+++ leetcode/triangle.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-     
-        vector<vector<int>> cost = triangle;
-        
+         // recursive sol would be to recurse through all possible paths
+        // take the minumum
+        // here we store the recursion up to the ith row jth col using DP
         // for i,j -> i-1,j-1 || i-1,j
-        
+        // here since we are updating the cost by adding the previous row costs
+        // we need not use any extra memeory 
+        // can do it in place
         if(triangle.size() == 1){
-            return triangle[0][0];
+            return triangle[0][0]; // if only 1 element then return that
         }
         
-        int r = 2;
-        cost[1][0] += cost[0][0];
-        cost[1][1] += cost[0][0];
-        
-        if(triangle.size() == 2){
-            return min(cost[1][0], cost[1][1]);
-        }
-        
-        
+        int r = 1;     // row index
         int res = INT_MAX;
-        while(r != triangle.size()){
-            
-            cost[r][0] += cost[r-1][0];
-            cost[r][triangle[r].size()-1] += cost[r-1][triangle[r].size()-2]; 
-            for(int c = 1; c < triangle[r].size()-1; c++){
-                cost[r][c] += min(cost[r-1][c-1], cost[r-1][c]);
+        int size = 0;
+        while(r != triangle.size()){ // iterate from triangel[1] -> triangle[n-1]
+            size = triangle[r].size(); // store size for repeated calc
+            triangle[r][0] += triangle[r-1][0]; // for the first and last indices only 1 way to acess
+            triangle[r][size-1] += triangle[r-1][size-2];  
+            for(int c = 1; c < size-1; c++){ // for all the compare the 2 costs of j and j-1 the col
+                triangle[r][c] += min(triangle[r-1][c-1], triangle[r-1][c]); // take minimum of the 2 and add the current cost
             }
-            r++;
+            r++; // go to next row
         }
-
-        for(int c = 0; c < cost[r-1].size(); c++){
-            res = min(res, cost[r-1][c]);
+        
+        // for last row scan thru the row
+        // take the minimum cost
+        for(int c = 0; c < size; c++){
+            res = min(res, triangle[r-1][c]);
         }
         
         return res;
