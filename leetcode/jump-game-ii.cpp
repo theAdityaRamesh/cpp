9c3eedb9eacd0b95b86405b5b099f946530f3c45
Aditya Ramesh <mail.aditya.ramesh@gmail.com>
2022-02-21 16:01:05 +0530

    Update jump-game-ii.cpp

--- leetcode/jump-game-ii.cpp
+++ leetcode/jump-game-ii.cpp
@@ -1,43 +1,53 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        
-        int last = nums.size()-1,
-            jump = 0,
-            indx = 0,
-            score = 0,
-            min_score = last,
-            jump_indx = 0;
-
-        if(nums.size() == 1 || nums[0] == 0){
+       
+        int size = nums.size();        
+        if(nums[0] == 0 || size == 1){
+            // if @ first place
+            // we cant jump
+            // return 0
+            // impossible case
             return 0;
-        } 
+        }
         
-        while(indx < last){
-            if(nums[indx] >= last){
-                jump++;
-                return jump;
-            } else {                 
-                for(int i = indx+1;i <=indx +nums[indx];i++){
-                    score = last - (i+nums[i]);
-                    
-                    if(score <= 0){
-                        jump += 2;
-                        return jump;
-                    } 
-                    
-                    if(score < min_score){
-                        min_score = score;
-                        jump_indx = i;
-                    }
-                }
-                    min_score = last;    
-                    indx = jump_indx;
-                    jump++;
+        if(nums[0] > size-1){
+            // if at first place max jump len is >
+            // than size of array
+            // we can cover array in single 
+            // jump
+            // return 1
+            return 1;
+        }
+        
+        
+        int jumps = 0;
+        int jumpIndx = nums[0];
+        int next = -1 ;
+        for(int i = 0; i < size-1; i++){
+            // @ every index assume that we jump the max possible
+            // calculate the next position based on that
+            // take max with the prev max jump pos
+            // based on prev indices
+            next = max(i+nums[i], next);
+            if(i == jumpIndx){
+                // if we reach the jumpIndx pos
+                // it means we have taken 1 jump
+                jumps++;
+                // update the jumpIndex with the next
+                // maximum possible index which is reacheable
+                jumpIndx = next;
             }
         }
         
-        return jump;
-        
+        // if @ end of array we are able to cross
+        // or reach last index
+        // we must have take one jump from prev
+        // max reaheable position
+        if(jumpIndx >= size-1){
+            return ++jumps;
+        }
+                
+        return jumps;
     }
 };
