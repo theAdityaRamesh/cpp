2ebcd0d787f875ba22b0c0a1033307b49c870ba2
Aditya Ramesh <mail.aditya.ramesh@gmail.com>
2022-03-01 09:38:05 +0530

    Update happy-number.cpp

--- leetcode/happy-number.cpp
+++ leetcode/happy-number.cpp
@@ -1,3 +1,44 @@
+// solution 1
+// use cycle detection
+// any to pointers which are moving with coprime speeds
+// will eventually meet each other moving in a cycle
+
+class Solution {
+public:
+    
+    int happyTransform(int n){
+        int squares = 0;
+        while(n){
+          squares +=  (n%10)*(n%10);
+          n /= 10;
+        }
+        
+        return squares;
+    }
+    
+    bool isHappy(int n) {
+        
+        int slow = n;
+        int fast = n;
+        while( fast != 1){
+            // slow pointer moves at 1 /iter
+            // fast pointer moves at 2 /iter
+            slow = happyTransform(slow);
+            fast = happyTransform(fast);
+            fast = happyTransform(fast);
+            // cout << slow << " " << fast << endl;
+            if(slow == fast){
+                break;
+            } 
+        }
+        
+        // if fast = 1 then its a happy num
+        // otherwise unhappy :(
+        return fast == 1;
+
+// solution 2
+// use a bit array to mark already visited nums
+
 class Solution {
 public:
     bool isHappy(int n) {
