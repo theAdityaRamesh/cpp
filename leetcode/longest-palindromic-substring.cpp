68529fd4efefdad2fcb6bfcaa2d7641a6947d528
Aditya Ramesh <mail.aditya.ramesh@gmail.com>
2022-03-07 08:44:05 +0530

    Update longest-palindromic-substring.cpp

--- leetcode/longest-palindromic-substring.cpp
+++ leetcode/longest-palindromic-substring.cpp
@@ -19,6 +19,20 @@ public:
     
     string longestPalindrome(string s) {
         
+        // we cam check all possible combinations of start and end
+        // that would be brute force
+        // but it would be O(nchoose2) or O(n^2)
+        // a better way is to avoid doing that
+        // and insetead check from the centre for each i
+        // if the whole string is a palindrom we dont ahve to go
+        // more than N/2
+        // this is still however O(n^2)
+        
+        // another approach is to have a matrix of size NxN
+        // we knoe that palindrom(i,j) is valid if palindrom(i-1,j-1) is valid and arr[i] == arr[j]
+        // now we can start checking all possible pairs using 2 loops
+        // and update max len if we find a larger palindrome
+        
         if(s == "" || s.size() == 1){
             string temp = "";
             temp += s[0];
